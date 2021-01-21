c41edc86451d3b21021886c1652b343162f8b08b
a-flying-pig <zhuhualoyal@163.com>
2021-01-21 08:09:02 +0800

    from think pad T470p, add unit6 codes   Changes to be committed:  	new file:   unit6_1_graph/Debug/graph_matrix.o  	new file:   unit6_1_graph/Debug/main.o  	new file:   unit6_1_graph/Debug/unit6_1_graph.exe  	new file:   unit6_1_graph/graph_matrix.c  	new file:   unit6_1_graph/graph_matrix.h  	new file:   unit6_1_graph/main.c  	new file:   unit6_1_graph_table/Debug/graph_table.o  	new file:   unit6_1_graph_table/Debug/main.o  	new file:   unit6_1_graph_table/Debug/unit6_1_graph_table.exe  	new file:   unit6_1_graph_table/graph_table.c  	new file:   unit6_1_graph_table/graph_table.h  	new file:   unit6_1_graph_table/main.c

---
+++ unit6_1_graph_table/main.c
@@ -0,0 +1,16 @@
+/*
+ * main.c
+ *
+ *  Created on: 2020Äê11ÔÂ25ÈÕ
+ *      Author: Administrator
+ */
+#include "graph_table.h"
+
+int main(int argc, char *argv[])
+{
+	setvbuf(stdout, NULL, _IONBF, 0);
+	printf("hello graph!");
+	return 0;
+}
+
+
